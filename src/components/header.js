10cb33493c609b922d3e409cc9b403b6dcd73226
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-03-10 20:57:04 -0600

    add searchbar class component

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Searchbar from './searchbar';
 
 class Header extends Component {
   render() {
     return (
       <div>
         Header
+        <Searchbar />
       </div>
     )
   }
---
+++ src/components/searchbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Searchbar extends Component {
+  render() {
+    return (
+      <div>
+        Searchbar
+      </div>
+    )
+  }
+}
+
+export default Searchbar;
