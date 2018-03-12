c8ba7711f1d672eb6304901a1f0c454bfb83c8fb
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-03-12 16:50:12 -0600

    add favorites actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,12 @@
-import { FETCH_TRENDING, RECEIVE_TRENDING } from './types';
+import {
+  FETCH_TRENDING,
+  RECEIVE_TRENDING,
+  FETCH_FAVORITES,
+  RECEIVE_FAVORITES,
+} from './types';
 
 export const requestApiTrending = () => ({ type: FETCH_TRENDING });
 export const receiveApiTrending = data => ({ type: RECEIVE_TRENDING, data });
+
+export const requestFavorites = () => ({ type: FETCH_FAVORITES});
+export const receiveFavorites = data => ({ type: RECEIVE_FAVORITES, data });
