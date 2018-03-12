5f62b5ce014fc1932319530e692bdda8bf9df790
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-03-12 16:53:31 -0600

    add mocked favorites

---
+++ src/data/favoriteGifs.js
@@ -0,0 +1,24 @@
+module.exports = {
+  data: [
+    {
+        "id": "vbQBut0QeHxrAn0bot",
+        "images": {
+            "original": {
+                "url": "https://media3.giphy.com/media/vbQBut0QeHxrAn0bot/giphy.gif",
+            },
+        },
+        "title": "cry crying GIF by Team Coco",
+        "isFavorite": true,
+    },
+    {
+        "id": "jVEtByVRrsD980HORR",
+        "images": {           
+            "original": {
+                "url": "https://media0.giphy.com/media/jVEtByVRrsD980HORR/giphy.gif",
+            },
+        },
+        "title": "after the final rose abc GIF by The Bachelor",
+        "isFavorite": true,
+    },
+  ]
+};
