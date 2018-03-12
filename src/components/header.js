ea68a46762df7d501a03597019ec3192546dd453
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-03-11 22:14:50 -0600

    add navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import Searchbar from './searchbar';
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        Header
+      <div class="header">
+        <NavLink to="/" activeClassName="active" exact>Trending</NavLink>
+        <NavLink to="/favorites" activeClassName="active">Favorites</NavLink>
         <Searchbar />
       </div>
     )
