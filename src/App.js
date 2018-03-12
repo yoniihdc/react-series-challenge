fe84686f6d4707a328492202942e488604a6e6f0
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-03-11 22:14:17 -0600

    Implement redux and router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/header';
 import Favorites from './components/favorites';
 import Details from './components/details';
-import Trending from './components/trending';
+import Trending from './components/trending/';
 import NotFound from './components/notFound';
+
+import { Provider } from 'react-redux';
+import store from './state/store';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Switch>
-            <Route path='/favorites' component={Favorites} exact />
-            <Route path='/details/:gifId' component={Details} exact />
-            <Route path='/' component={Trending} exact />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Header />
+            <Switch>
+              <Route path='/favorites' component={Favorites} exact />
+              <Route path='/details/:gifId' component={Details} exact />
+              <Route path='/' component={Trending} exact />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
     )
   }
 }
