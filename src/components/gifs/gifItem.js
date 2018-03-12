bf7e5763e76850fcb0ed23974d6ed226fd1cc59e
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-03-12 16:52:34 -0600

    add favorites validation

--- src/components/gifs/gifItem.js
+++ src/components/gifs/gifItem.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const GifCard = ({ gif }) => {
-  return (
-    <div className="card">
-      <img src={gif.images.original.url} alt={gif.id} />
+class GifCard extends Component {
+  renderAddButton() {
+    return (
       <div className="fav-button">
         <button>Add to ♥</button>
       </div>
-    </div>
-  );
-};
+    );
+  }
+
+  renderRemoveButton() {
+    return (
+      <div className="fav-button">
+        <button>Remove from ♥</button>
+      </div>
+    );
+  }
+
+  render() {
+    const { gif } = this.props;
+
+    return (
+      <div className="card">
+        <img src={gif.images.original.url} alt={gif.id} />
+        {gif.isFavorite ? this.renderRemoveButton() : this.renderAddButton()}
+      </div>
+    );
+  }
+}
 
 export default GifCard;
